01757eefde38281bbfbdd9a2be9dd993f294cd80
Matteo Murgida <teomurgi@gmail.com>
2025-02-01 14:58:01 +0100

    load save iventory

--- 00_Globals/global_save_manager.gd
+++ 00_Globals/global_save_manager.gd
@@ -23,6 +23,7 @@ var current_save: Dictionary = {
 func save_game() -> void:
 	update_player_data()
 	update_scene_path()
+	update_item_data()
 	var file := FileAccess.open(SAVE_PATH + "save.sav", FileAccess.WRITE)
 	var save_json = JSON.stringify(current_save)
 	file.store_line(save_json)
@@ -41,6 +42,7 @@ func load_game() -> void:
 	await LevelManager.level_load_started
 	PlayerManager.set_player_position(Vector2(current_save.player.pos_x, current_save.player.pos_y))
 	PlayerManager.set_health(current_save.player.hp, current_save.player.max_hp)
+	PlayerManager.INVENTORY_DATA.parse_save_data(current_save.items)
 	PlayerManager.reset_to_cardinal_direction(Vector2(current_save.player.card_dir_x, current_save.player.card_dir_y))
 	await LevelManager.level_loaded
 	game_loaded.emit()
@@ -61,3 +63,6 @@ func update_scene_path() -> void:
 			p = c.scene_file_path
 			break
 	current_save.scene_path = p
+
+func update_item_data() -> void:
+	current_save.items = PlayerManager.INVENTORY_DATA.get_save_data()
