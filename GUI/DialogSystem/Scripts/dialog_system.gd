9790cc43e69381b037add9a152249894f5fcd483
Matteo Murgida <teomurgi@gmail.com>
2025-02-15 15:06:05 +0100

    e365: branching dialog

--- GUI/DialogSystem/Scripts/dialog_system.gd
+++ GUI/DialogSystem/Scripts/dialog_system.gd
@@ -7,6 +7,7 @@ signal letter_added(letter: String)
 
 var is_active: bool = false
 var text_in_progress: bool = false
+var waiting_for_choice: bool = false
 
 var text_speed: float = 0.02
 var text_length: int = 0
@@ -24,92 +25,120 @@ var dialog_item_index: int = 0
 @onready var dialog_progress_indicator_label: Label = $DialogUI/DialogProgressIndicator/Label
 @onready var timer: Timer = $DialogUI/Timer
 @onready var audio_stream_player: AudioStreamPlayer = $DialogUI/AudioStreamPlayer
+@onready var choice_options: VBoxContainer = $DialogUI/VBoxContainer
 
 func _ready() -> void:
-    if Engine.is_editor_hint():
-        if get_viewport() is Window:
-            get_parent().remove_child(self)
-            return
-        return
-    timer.timeout.connect(_on_timer_timeout)
-    hide_dialog()
+	if Engine.is_editor_hint():
+		if get_viewport() is Window:
+			get_parent().remove_child(self)
+			return
+		return
+	timer.timeout.connect(_on_timer_timeout)
+	hide_dialog()
 
 
 func _unhandled_input(event: InputEvent) -> void:
-    if is_active == false:
-        return
-    if(
-        event.is_action_pressed("interact") or
-        event.is_action_pressed("ui_accept") or
-        event.is_action_pressed("attack")
-    ):
-        if text_in_progress == true:
-            content.visible_characters = text_length
-            return
-        dialog_item_index += 1
-        if dialog_item_index < dialog_items.size():
-            start_dialog()
-        else:
-            hide_dialog()
+	if is_active == false:
+		return
+	if(
+		event.is_action_pressed("interact") or
+		event.is_action_pressed("ui_accept") or
+		event.is_action_pressed("attack")
+	):
+		if text_in_progress == true:
+			content.visible_characters = text_length
+			return
+		elif waiting_for_choice == true:
+			return
+		dialog_item_index += 1
+		if dialog_item_index < dialog_items.size():
+			start_dialog()
+		else:
+			hide_dialog()
 
 func show_dialog(items: Array[DialogItem]) -> void:
-    is_active = true
-    dialog_ui.visible = true
-    dialog_ui.process_mode = Node.PROCESS_MODE_ALWAYS
-    dialog_items = items
-    dialog_item_index = 0
-    get_tree().paused = true
-    await get_tree().process_frame
-    start_dialog()
+	is_active = true
+	dialog_ui.visible = true
+	dialog_ui.process_mode = Node.PROCESS_MODE_ALWAYS
+	dialog_items = items
+	dialog_item_index = 0
+	get_tree().paused = true
+	await get_tree().process_frame
+	start_dialog()
 
 func hide_dialog() -> void:
-    is_active = false
-    dialog_ui.visible = false
-    dialog_ui.process_mode = Node.PROCESS_MODE_DISABLED
-    get_tree().paused = false
-    finished.emit()
+	is_active = false
+	choice_options.visible = false
+	dialog_ui.visible = false
+	dialog_ui.process_mode = Node.PROCESS_MODE_DISABLED
+	get_tree().paused = false
+	finished.emit()
 
 func start_dialog() -> void:
-    show_dialog_button_indicator(false)
-    var _d: DialogItem = dialog_items[dialog_item_index]
-    set_dialog_data(_d)
-    content.visible_characters = 0
-    text_length = content.get_total_character_count()
-    plain_text = content.get_parsed_text()
-    text_in_progress = true
-    start_timer()
+	waiting_for_choice = false
+	show_dialog_button_indicator(false)
+	var _d: DialogItem = dialog_items[dialog_item_index]
+	if _d is DialogText:
+		set_dialog_text(_d)
+	elif _d is DialogChoice:
+		set_dialog_choice(_d)
+
+
+
 
 
 func show_dialog_button_indicator(_is_visible: bool) -> void:
-    dialog_progress_indicator.visible = _is_visible
-    if dialog_item_index == dialog_items.size() - 1:
-        dialog_progress_indicator_label.text = "END"
-    else:
-        dialog_progress_indicator_label.text = "NEXT"
+	dialog_progress_indicator.visible = _is_visible
+	if dialog_item_index == dialog_items.size() - 1:
+		dialog_progress_indicator_label.text = "END"
+	else:
+		dialog_progress_indicator_label.text = "NEXT"
 
 
-func set_dialog_data(d: DialogItem) -> void:
-    if d is DialogText:
-        content.text = d.text
-    name_label.text = d.npc_info.npc_name
-    portrait_sprite.texture = d.npc_info.portrait
-    portrait_sprite.audio_pitch_base = d.npc_info.dialog_audio_pitch
+func set_dialog_text(d: DialogText) -> void:
+	content.text = d.text
+	name_label.text = d.npc_info.npc_name
+	portrait_sprite.texture = d.npc_info.portrait
+	portrait_sprite.audio_pitch_base = d.npc_info.dialog_audio_pitch
+	content.visible_characters = 0
+	text_length = content.get_total_character_count()
+	plain_text = content.get_parsed_text()
+	text_in_progress = true
+	start_timer()
+
+func set_dialog_choice(d: DialogChoice) -> void:
+	choice_options.visible = true
+	waiting_for_choice = true
+	for c in choice_options.get_children():
+		c.queue_free()
+	for i in d.dialog_branches.size():
+		var _new_choice: Button = Button.new()
+		_new_choice.text = d.dialog_branches[i].text
+		_new_choice.alignment = HORIZONTAL_ALIGNMENT_LEFT
+		_new_choice.pressed.connect(_dialog_choice_selected.bind(d.dialog_branches[i]))
+		choice_options.add_child(_new_choice)
+	await get_tree().process_frame
+	choice_options.get_child(0).grab_focus()
+
+func _dialog_choice_selected(_d: DialogBranch) -> void:
+	choice_options.visible = false
+	show_dialog(_d.dialog_items)
 
 func start_timer() -> void:
-    timer.wait_time = text_speed
-    # Manipulate wait_time
-    var _char = plain_text[content.visible_characters - 1]
-    if '.,!?:;'.contains(_char):
-        timer.wait_time *= 4
-    elif ', '.contains(_char):
-        timer.wait_time *= 2
-    timer.start()
+	timer.wait_time = text_speed
+	# Manipulate wait_time
+	var _char = plain_text[content.visible_characters - 1]
+	if '.,!?:;'.contains(_char):
+		timer.wait_time *= 4
+	elif ', '.contains(_char):
+		timer.wait_time *= 2
+	timer.start()
 
 func _on_timer_timeout() -> void:
-    content.visible_characters += 1
-    if content.visible_characters <= text_length:
-        letter_added.emit(plain_text[content.visible_characters - 1])
-        start_timer()
-    else:
-        show_dialog_button_indicator(true)
-        text_in_progress = false
+	content.visible_characters += 1
+	if content.visible_characters <= text_length:
+		letter_added.emit(plain_text[content.visible_characters - 1])
+		start_timer()
+	else:
+		show_dialog_button_indicator(true)
+		text_in_progress = false
--- Interactables/Dialog/Scripts/dialog_choice.gd
+++ Interactables/Dialog/Scripts/dialog_choice.gd
@@ -2,4 +2,27 @@
 @icon("res://GUI/DialogSystem/Icons/question_bubble.svg")
 class_name DialogChoice extends DialogItem
 
-@export var choices: Array[String] = ["Yes", "No"]
+var dialog_branches: Array[DialogBranch]
+
+func _ready() -> void:
+    if Engine.is_editor_hint():
+        return
+    for c in get_children():
+        if c is DialogBranch:
+            dialog_branches.append(c)
+            
+func _get_configuration_warnings() -> PackedStringArray:
+    if not _check_for_dialog_branches():
+        return ["Requires at least two DialogBranch children."]
+    else:
+        return []
+
+
+func _check_for_dialog_branches() -> bool:
+    var count:int = 0
+    for c in get_children():
+        if c is DialogBranch:
+           count += 1
+           if count == 2:
+               return true
+    return false
